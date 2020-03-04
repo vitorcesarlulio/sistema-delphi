48d449a624bcfe6dbe9483b6a88428181a9cd04c
Vitor Cesar Lulio <vitorcesardtb1@hotmail.com>
2020-03-04 20:49:36 -0300

    Aula 04-03

--- fontes/u_frm_modelo.pas
+++ fontes/u_frm_modelo.pas
@@ -4,10 +4,30 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.ComCtrls,
+  Vcl.StdCtrls, Vcl.Buttons, Data.DB, Vcl.DBCtrls, Vcl.Grids, Vcl.DBGrids;
 
 type
   Tfrm_modelo = class(TForm)
+    pgcModelo: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    Panel1: TPanel;
+    btnNovo: TBitBtn;
+    btnSalvar: TBitBtn;
+    btnCancelar: TBitBtn;
+    btnApagar: TBitBtn;
+    btnAlterar: TBitBtn;
+    btnImprimir: TBitBtn;
+    btnFechar: TBitBtn;
+    DBNav: TDBNavigator;
+    Panel2: TPanel;
+    rgModelo: TRadioGroup;
+    edtModelo: TEdit;
+    btnAbrir: TBitBtn;
+    dbgModelo: TDBGrid;
+    dsDados: TDataSource;
+    lbCodigo: TLabel;
   private
     { Private declarations }
   public
--- fontes/u_frm_principal.dfm
+++ fontes/u_frm_principal.dfm
@@ -2,8 +2,8 @@ object frm_principal: Tfrm_principal
   Left = 0
   Top = 0
   Caption = 'Formul'#225'rio menu'
-  ClientHeight = 201
-  ClientWidth = 447
+  ClientHeight = 314
+  ClientWidth = 568
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
